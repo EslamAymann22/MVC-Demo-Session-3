3dffd50dc3b5fc8a90ce72302c87408b90835831
Eslam Ayman <Eslam.aymann.22a@gmail.com>
2024-10-31 00:21:43 +0300

    Add Employee Model

--- Demo.PL/obj/Debug/net5.0/Demo.PL.RazorTargetAssemblyInfo.cs
+++ Demo.PL/obj/Debug/net5.0/Demo.PL.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.PL")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b8638876dc4d43fd937dba9614a82660d15f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8057d30cda276a94f9a1fc318d2f8aa54a51705b")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.PL.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
